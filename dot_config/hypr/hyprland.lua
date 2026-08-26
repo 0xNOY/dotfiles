27ef99b5c207bee77fd6579dfd6395c24aafae1c
0xNOY <noy@abc-net.jp>
2026-08-26 13:55:39 +0900

    feat(hyprland): use nonlinear brightness steps

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -192,10 +192,10 @@ hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 hl.bind("XF86MonBrightnessUp",
-    hl.dsp.exec_cmd("brightnessctl set +5%"),
+    hl.dsp.exec_cmd("brightnessctl --exponent set +5%"),
     { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown",
-    hl.dsp.exec_cmd("brightnessctl set 5%-"),
+    hl.dsp.exec_cmd("brightnessctl --exponent set 5%-"),
     { locked = true, repeating = true })
 hl.bind("XF86AudioRaiseVolume",
     hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+"),
